a4eb81de5de43dc7be5874ba739c96861e84b380
Keisuke Nakayama <baskk4.dt@gmail.com>
2020-05-06 20:55:09 +0900

    Add .gimonfu.json

--- src/index.ts
+++ src/index.ts
@@ -2,33 +2,64 @@ import postArticleAsync from './post'
 
 const { program } = require('commander')
 const packageJson = require('../package.json')
+import path from "path"
+import { promises as fs } from 'fs'
 
-// Commandline arguments
-program
-  .version(packageJson.version)
-  .requiredOption('-f --file <path>', 'post a article of markdown file')
-  .option('-c --custom-url <string>','specify custom-url to post\n( ex: https://<user>.hatenablog.com/entry/<string> )')
-  .option('-u --user <string>','hatena ID (prioritize over environment variable "HATENA_USER")')
-  .option('-p --password <string>','hatena API key (prioritize over environment variable "HATENA_API_KEY")')
-  .option('-b --blog-id <string>','blog ID (prioritize over environment variable "HATENA_BLOG_ID")')
+const findConfigPathRecurusive = (dirPath: string): Promise<string> => {
+  const configPath = path.resolve( dirPath, '.gimonfu.json' )
+  return fs.readFile(configPath, 'utf-8').catch( ()=> {
+    const parentDir = path.resolve( dirPath, '../' )
+    if ( parentDir === dirPath ) {
+      console.error(`Need .gimonfu.json`)
+      process.exit(-1)
+    }
+    return findConfigPathRecurusive( parentDir )
+  })
 
-program.parse(process.argv)
+}
+const gimonfuJson = '.gimonfu.json'
+const loadConfig = async () => {
 
-const user = program.user || process.env.HATENA_USER
-const password = program.password ||  process.env.HATENA_API_KEY
-const blogId = program.blogId || process.env.HATENA_BLOG_ID
-const filePath = program.file
-const articlePath = program.customUrl
+  const configString: string = await findConfigPathRecurusive( process.cwd() )
 
-// Validation
-if ( !user || !password || !blogId || !filePath ) {
-  console.error('Need user, password, blog-id, markdown-file-path')
-  process.exit(-1)
+  try {
+  const config = JSON.parse(configString)
+    if( !config?.user_id || !config?.blog_id || !config?.api_key ) {
+      console.error(`Need user_id, blog_id, api_key in .gimonfu.json`)
+      process.exit(-1)
+    }
+    return config
+  } catch {
+    console.error(`Failed to parse .gimonfu.json`)
+    process.exit(-1)
+  }
 }
 
-// Post
-postArticleAsync(user, password, blogId, filePath, articlePath)
-  .then( () => {
-    console.log('Success')
-})
+const main = async () => {
+  // Commandline arguments
+  program
+    .version(packageJson.version)
+    .option('-f --file <path>', 'post a article of markdown file')
+    .option('-c --custom-url <string>','specify custom-url to post\n( ex: https://<user>.hatenablog.com/entry/<string> )')
+
+  program.parse(process.argv)
+
+
+  const {user_id: user, api_key: password, blog_id: blogId} = await loadConfig()
+
+  const filePath = program.file
+  const articlePath = program.customUrl
+
+  // Validation
+  if ( !user || !password || !blogId || !filePath ) {
+    console.error('Need user, password, blog-id, markdown-file-path')
+    process.exit(-1)
+  }
+
+  // Post
+  await postArticleAsync(user, password, blogId, filePath, articlePath)
+  console.log('Success')
+}
+
+main()
 
