2bdd358b2768ae2be9955a400f567648629860a9
Keisuke Nakayama <baskk4.dt@gmail.com>
2020-05-06 21:19:10 +0900

    Refactoring

--- src/index.ts
+++ src/index.ts
@@ -5,32 +5,30 @@ const packageJson = require('../package.json')
 import path from "path"
 import { promises as fs } from 'fs'
 
-const findConfigPathRecurusive = (dirPath: string): Promise<string> => {
+const loadConfigFile = (dirPath: string): Promise<string> => {
   const configPath = path.resolve( dirPath, '.gimonfu.json' )
   return fs.readFile(configPath, 'utf-8').catch( ()=> {
     const parentDir = path.resolve( dirPath, '../' )
     if ( parentDir === dirPath ) {
-      console.error(`Need .gimonfu.json`)
+      // root directory までさかのぼっても .gimonfu.jsonが無い
+      console.error('Need .gimonfu.json')
       process.exit(-1)
     }
-    return findConfigPathRecurusive( parentDir )
+    return loadConfigFile( parentDir )
   })
-
 }
-const gimonfuJson = '.gimonfu.json'
+
 const loadConfig = async () => {
-
-  const configString: string = await findConfigPathRecurusive( process.cwd() )
-
+  const configString: string = await loadConfigFile( process.cwd() )
   try {
-  const config = JSON.parse(configString)
+    const config = JSON.parse(configString)
     if( !config?.user_id || !config?.blog_id || !config?.api_key ) {
-      console.error(`Need user_id, blog_id, api_key in .gimonfu.json`)
+      console.error('Need user_id, blog_id, api_key in .gimonfu.json')
       process.exit(-1)
     }
     return config
   } catch {
-    console.error(`Failed to parse .gimonfu.json`)
+    console.error('Failed to parse .gimonfu.json')
     process.exit(-1)
   }
 }
@@ -39,22 +37,14 @@ const main = async () => {
   // Commandline arguments
   program
     .version(packageJson.version)
-    .option('-f --file <path>', 'post a article of markdown file')
+    .requiredOption('-f --file <path>', 'post a article of markdown file')
     .option('-c --custom-url <string>','specify custom-url to post\n( ex: https://<user>.hatenablog.com/entry/<string> )')
 
   program.parse(process.argv)
 
-
   const {user_id: user, api_key: password, blog_id: blogId} = await loadConfig()
-
   const filePath = program.file
   const articlePath = program.customUrl
-
-  // Validation
-  if ( !user || !password || !blogId || !filePath ) {
-    console.error('Need user, password, blog-id, markdown-file-path')
-    process.exit(-1)
-  }
 
   // Post
   await postArticleAsync(user, password, blogId, filePath, articlePath)
