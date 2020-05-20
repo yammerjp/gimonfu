6d4d1f42773e436bc48a1ab0a225a99cdea16a6b
Keisuke Nakayama <baskk4.dt@gmail.com>
2020-05-21 02:57:39 +0900

    Refactoring

--- src/push.ts
+++ src/push.ts
@@ -3,71 +3,124 @@ import startup from './startup'
 import AtomPubRequest from './atomPubRequest'
 import FileRequest from './fileRequest'
 
+interface Request {
+  atomPub: AtomPubRequest
+  file: FileRequest
+}
+
+interface UploadResult {
+  article: Article
+  uploaded: boolean
+  error?: UploadError
+}
+
+type UploadErrorType = 'CONFLICT' | 'INVALID_ID'
+
+interface UploadError {
+  type: UploadErrorType
+  message: string
+}
+
 export default async function (options: ReadOptions) {
   const { atomPubRequest, fileRequest } = await startup()
 
   const remoteArticles = await atomPubRequest.fetchs()
   const localArticles = await fileRequest.reads(options)
 
-  await Promise.all(
+  const results = await Promise.all(
     localArticles.map( localArticle =>
       uploadNewerArticle(
         localArticle,
         remoteArticles,
         localArticles,
-        atomPubRequest,
-        fileRequest
+        { atomPub: atomPubRequest, file: fileRequest }
   )))
+
+  results.filter( r => r.uploaded ).forEach( result => {
+    console.log(`Upload: ${atomPubRequest.fullUrl(result.article.customUrl)}`)
+  })
+
+  results.filter( r => r.error ).forEach( result => {
+    console.log(result.error?.message)
+  })
+
 }
 
 const uploadNewerArticle = async (
   localArticle: Article,
   remoteArticles: Article[],
   localArticles: Article[],
-  atomPubRequest: AtomPubRequest,
-  fileRequest: FileRequest
-) => {
+  request: Request
+): Promise<UploadResult> => {
+
   const conflictRemoteArticle = remoteArticles.find( remote =>
     remote.id !== localArticle.id && remote.customUrl === localArticle.customUrl
   )
   if( conflictRemoteArticle !== undefined ){
-    // Conflict: idの違うカスタムURLの同じ記事が既にリモートに存在する
-    console.log(`Skip to upload/update, because of CONFLICT`)
-    console.log(` local:  ${fileRequest.customUrl2filePath(localArticle.customUrl)}`)
-    console.log(` remote: ${atomPubRequest.fullUrl(conflictRemoteArticle.customUrl)}`)
-    console.log(` If you want to overwrite the remote article, Please delete it.`)
-    return
+    return conflictResult( localArticle, conflictRemoteArticle, request )
   }
 
   if( localArticle.id === undefined ){
-    // post new article
-    const newArticle = await atomPubRequest.post(localArticle)
-    // 投稿に成功した内容でローカルのファイルを上書き
-    await fileRequest.delete(localArticle)
-    await fileRequest.write(newArticle)
-    console.log(`Upload: ${atomPubRequest.fullUrl(localArticle.customUrl)}`)
-    return
+    const newArticle = await request.atomPub.post(localArticle)
+    await request.file.delete(localArticle)
+    await request.file.write(newArticle)
+    return { article: newArticle, uploaded: true }
   }
 
   const remoteArticle = remoteArticles.find( remote =>
     remote.id === localArticle.id
   )
   if( remoteArticle === undefined ) {
-    console.log(`Skip to update, because of invalid id`)
-    console.log(` local:  ${fileRequest.customUrl2filePath(localArticle.customUrl)}`)
-    console.log(` If you want to upload the local file, Please delete id in the file's front-matter.`)
-    return
+    return invalidIdResult( localArticle, request )
   }
 
   const compare = new Compare(localArticle, remoteArticle)
   if(compare.remoteIsNew() || compare.sameWithoutEditedDate()) {
-    return
+    return { article: localArticle, uploaded: false }
   }
 
-  // Update article
-  const newArticle = await atomPubRequest.put(localArticle)
-  // 投稿に成功した内容でローカルのファイルを上書き
-  await fileRequest.delete(localArticle)
-  await fileRequest.write(newArticle)
-  console.log(`Update: ${atomPubRequest.fullUrl(localArticle.customUrl)}`)
+  const newArticle = await request.atomPub.put(localArticle)
+  await request.file.delete(localArticle)
+  await request.file.write(newArticle)
+  return { article: newArticle, uploaded: true }
 }
+
+const conflictErrorMessage = ( localPath: string, url: string ): string =>
+`Skip to upload/update, because of CONFLICT
+ local:  ${localPath}
+ remote: ${url}
+ If you want to overwrite the remote article, Please delete it.`
+
+
+const invalidIdErrorMessage = (localPath: string): string =>
+`Skip to update, because of invalid id
+ local:  ${localPath}
+ If you want to upload the local file, Please delete id in the file's front-matter.`
+
+const conflictResult = (
+  local: Article,
+  remote: Article,
+  request: Request
+): UploadResult => ({
+  article: local,
+  uploaded: false,
+  error: {
+    type: 'CONFLICT',
+    message: conflictErrorMessage(
+      request.file.customUrl2filePath(local.customUrl),
+      request.atomPub.fullUrl(remote.customUrl)
+    )
+  }
+})
+
+const invalidIdResult = (
+  local: Article,
+  request: Request
+): UploadResult => ({
+  article: local,
+  uploaded: false,
+  error: {
+  type: "INVALID_ID",
+    message: invalidIdErrorMessage(request.file.customUrl2filePath(local.customUrl))
+  }
+})
