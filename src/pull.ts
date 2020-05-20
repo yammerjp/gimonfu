361399dfb8ec340aa3c7d04739ae82b10dceabc9
Keisuke Nakayama <baskk4.dt@gmail.com>
2020-05-21 00:22:20 +0900

    Refactoring

--- src/pull.ts
+++ src/pull.ts
@@ -9,53 +9,60 @@ export default async function (options: ReadOptions) {
   const remoteArticles = await atomPubRequest.fetchs()
   const localArticles = await fileRequest.reads(options)
 
-  await downloadNewerArticles( remoteArticles, localArticles, atomPubRequest, fileRequest )
+  await Promise.all(
+    remoteArticles.map( remoteArticle =>
+      downloadNewerArticle(
+        remoteArticle,
+        remoteArticles,
+        localArticles,
+        atomPubRequest,
+        fileRequest
+  )))
 }
 
-const downloadNewerArticles = async (
+const downloadNewerArticle = async (
+  remoteArticle: Article,
   remoteArticles: Article[],
   localArticles: Article[],
   atomPubRequest: AtomPubRequest,
   fileRequest: FileRequest
 ) => {
-  await Promise.all( remoteArticles.map( async remoteArticle => {
-    const localArticle = localArticles.find( local =>
-      local.id === remoteArticle.id
-    )
+  const conflictLocalArticle = localArticles.find( local =>
+    local.id !== remoteArticle.id && local.customUrl === remoteArticle.customUrl
+  )
+  if( conflictLocalArticle !== undefined ){
+    // Conflict: idの違うファイルが既にローカルに存在する
+    console.log(`Skip to update, because of CONFLICT`)
+    console.log(` local:  ${fileRequest.customUrl2filePath(conflictLocalArticle.customUrl)}`)
+    console.log(` remote: ${atomPubRequest.fullUrl(remoteArticle.customUrl)}`)
+    console.log(` If you want to overwrite the local file, Please delete it.`)
+    return
+  }
 
-    const conflictLocalArticle = localArticles.find( local => 
-      local.id !== remoteArticle.id && local.customUrl === remoteArticle.customUrl
-    )
-    if( conflictLocalArticle !== undefined ){
-      // Conflict: idの違うファイルが既にローカルに存在する
-      console.log(`Skip to update, because of CONFLICT`)
-      console.log(` local:  ${fileRequest.customUrl2filePath(conflictLocalArticle.customUrl)}`)
-      console.log(` remote: ${atomPubRequest.fullUrl(remoteArticle.customUrl)}`)
-      console.log(` If you want to overwrite the local file, Please delete it.`)
-      return
-    }
+  const localArticle = localArticles.find( local =>
+    local.id === remoteArticle.id
+  )
 
-    if( localArticle === undefined ) {
-      // Download new article
-      await fileRequest.write(remoteArticle)
-      console.log(`Download: ${fileRequest.customUrl2filePath(remoteArticle.customUrl)}`)
-      return
-    }
-
-    const compare = new Compare(localArticle, remoteArticle)
-    if(compare.localIsNew() || compare.sameWithoutEditedDate()) {
-      return
-    }
-
-    // Update article
-    let messageHead = ''
-    if( localArticle.customUrl !== remoteArticle.customUrl ){
-      // ファイルが移動していた場合
-      await fileRequest.delete(localArticle)
-      messageHead = `${fileRequest.customUrl2filePath(localArticle.customUrl)}\n     -> `
-    }
-
+  if( localArticle === undefined ) {
+    // Download new article
     await fileRequest.write(remoteArticle)
-    console.log(`Update: ${messageHead}${fileRequest.customUrl2filePath(remoteArticle.customUrl)}`)
-  }))
+    console.log(`Download: ${fileRequest.customUrl2filePath(remoteArticle.customUrl)}`)
+    return
+  }
+
+  const compare = new Compare(localArticle, remoteArticle)
+  if(compare.localIsNew() || compare.sameWithoutEditedDate()) {
+    return
+  }
+
+  // Update article
+  let messageHead = ''
+  if( localArticle.customUrl !== remoteArticle.customUrl ){
+    // ファイルが移動していた場合
+    await fileRequest.delete(localArticle)
+    messageHead = `${fileRequest.customUrl2filePath(localArticle.customUrl)}\n     -> `
+  }
+
+  await fileRequest.write(remoteArticle)
+  console.log(`Update: ${messageHead}${fileRequest.customUrl2filePath(remoteArticle.customUrl)}`)
 }
--- src/push.ts
+++ src/push.ts
@@ -9,58 +9,65 @@ export default async function (options: ReadOptions) {
   const remoteArticles = await atomPubRequest.fetchs()
   const localArticles = await fileRequest.reads(options)
 
-  await uploadNewerArticles( remoteArticles, localArticles, atomPubRequest, fileRequest )
+  await Promise.all(
+    localArticles.map( localArticle =>
+      uploadNewerArticle(
+        localArticle,
+        remoteArticles,
+        localArticles,
+        atomPubRequest,
+        fileRequest
+  )))
 }
 
-const uploadNewerArticles = async (
+const uploadNewerArticle = async (
+  localArticle: Article,
   remoteArticles: Article[],
   localArticles: Article[],
   atomPubRequest: AtomPubRequest,
   fileRequest: FileRequest
 ) => {
-  return await Promise.all( localArticles.map( async localArticle => {
-    const conflictRemoteArticle = remoteArticles.find( remote => 
-      remote.id !== localArticle.id && remote.customUrl === localArticle.customUrl
-    )
-    if( conflictRemoteArticle !== undefined ){
-      // Conflict: idの違うカスタムURLの同じ記事が既にリモートに存在する
-      console.log(`Skip to upload/update, because of CONFLICT`)
-      console.log(` local:  ${fileRequest.customUrl2filePath(localArticle.customUrl)}`)
-      console.log(` remote: ${atomPubRequest.fullUrl(conflictRemoteArticle.customUrl)}`)
-      console.log(` If you want to overwrite the remote article, Please delete it.`)
-      return
-    }
+  const conflictRemoteArticle = remoteArticles.find( remote =>
+    remote.id !== localArticle.id && remote.customUrl === localArticle.customUrl
+  )
+  if( conflictRemoteArticle !== undefined ){
+    // Conflict: idの違うカスタムURLの同じ記事が既にリモートに存在する
+    console.log(`Skip to upload/update, because of CONFLICT`)
+    console.log(` local:  ${fileRequest.customUrl2filePath(localArticle.customUrl)}`)
+    console.log(` remote: ${atomPubRequest.fullUrl(conflictRemoteArticle.customUrl)}`)
+    console.log(` If you want to overwrite the remote article, Please delete it.`)
+    return
+  }
 
-    if( localArticle.id === undefined ){
-      // post new article
-      const newArticle = await atomPubRequest.post(localArticle)
-      // 投稿に成功した内容でローカルのファイルを上書き
-      await fileRequest.delete(localArticle)
-      await fileRequest.write(newArticle)
-      console.log(`Upload: ${atomPubRequest.fullUrl(localArticle.customUrl)}`)
-      return
-    }
-
-    const remoteArticle = remoteArticles.find( remote =>
-      remote.id === localArticle.id
-    )
-    if( remoteArticle === undefined ) {
-      console.log(`Skip to update, because of invalid id`)
-      console.log(` local:  ${fileRequest.customUrl2filePath(localArticle.customUrl)}`)
-      console.log(` If you want to upload the local file, Please delete id in the file's front-matter.`)
-      return
-    }
-
-    const compare = new Compare(localArticle, remoteArticle)
-    if(compare.remoteIsNew() || compare.sameWithoutEditedDate()) {
-      return
-    }
-
-    // Update article
-    const newArticle = await atomPubRequest.put(localArticle)
+  if( localArticle.id === undefined ){
+    // post new article
+    const newArticle = await atomPubRequest.post(localArticle)
     // 投稿に成功した内容でローカルのファイルを上書き
     await fileRequest.delete(localArticle)
     await fileRequest.write(newArticle)
-    console.log(`Update: ${atomPubRequest.fullUrl(localArticle.customUrl)}`)
-  }))
+    console.log(`Upload: ${atomPubRequest.fullUrl(localArticle.customUrl)}`)
+    return
+  }
+
+  const remoteArticle = remoteArticles.find( remote =>
+    remote.id === localArticle.id
+  )
+  if( remoteArticle === undefined ) {
+    console.log(`Skip to update, because of invalid id`)
+    console.log(` local:  ${fileRequest.customUrl2filePath(localArticle.customUrl)}`)
+    console.log(` If you want to upload the local file, Please delete id in the file's front-matter.`)
+    return
+  }
+
+  const compare = new Compare(localArticle, remoteArticle)
+  if(compare.remoteIsNew() || compare.sameWithoutEditedDate()) {
+    return
+  }
+
+  // Update article
+  const newArticle = await atomPubRequest.put(localArticle)
+  // 投稿に成功した内容でローカルのファイルを上書き
+  await fileRequest.delete(localArticle)
+  await fileRequest.write(newArticle)
+  console.log(`Update: ${atomPubRequest.fullUrl(localArticle.customUrl)}`)
 }
