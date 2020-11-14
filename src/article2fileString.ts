1eb66b08b9ff836e0d8634981e895fe82e3db9a9
Keisuke Nakayama <baskk4.dt@gmail.com>
2020-11-14 20:58:14 +0900

    Fix to detoxification colon in title

--- src/article2fileString.ts
+++ src/article2fileString.ts
@@ -4,7 +4,7 @@
     '' : ['\ncategories:', ...article.categories].join('\n  - ')
 
   return `---\n`
-    +    `title: ${article.title}\n`
+    +    `title: ${detox(article.title)}\n`
     +    `date: ${article.date.toISOString()}${categoriesString}\n`
     +    `id: "${article.id}"\n`
     +    `draft: ${article.draft}\n`
@@ -12,4 +12,12 @@
     +    `${article.text}`
   // idは数字のみで構成された文字列だが、""をつけて文字列であることを明示して記録
   // 無いと読み取り時に数字として解釈され、その上で値が2つほど前後する。(原因未調査)
+}
+
+const detox = (title: string) :string => {
+  if (title.indexOf(':') == -1) {
+    return title
+  }
+  title = '"' + title.replace(/"/g, '\\"') + '"'
+  return title
 }
